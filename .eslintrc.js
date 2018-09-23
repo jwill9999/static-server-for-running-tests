e972b51cfaf885605910644877aad215ad03852a
Jason Williams <jasonwilliams2308@gmail.com>
2018-09-23 19:34:07 +0100

    eslint config file updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,14 +7,16 @@ module.exports = {
         "airbnb-base",
         "prettier"
         ],
-        "plugins": [           
+        "plugins": [   
+            "import",        
             "prettier"  
           ],
         "rules": {
            "prettier/prettier": "error",
            "linebreak-style": ["error", "windows"],
            "no-console": 0,
-           "radix": 0
+           "radix": 0,
+           "no-param-reassign": 0
            
         },
         "overrides": [
